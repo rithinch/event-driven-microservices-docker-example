7770ea3d76cf3ed4bc2fae300f9d311604033dc8
rithinch <Rithin@Rithins-MacBook-Pro.local>
2018-11-19 23:43:12 +0000

    Added Environment Variables

--- src/services/articles-management/environment/config.js
+++ src/services/articles-management/environment/config.js
@@ -1,9 +1,14 @@
 let config = {
-    dbEndpoint = "",
-    dbAuthKey = "",
-    servicePort = "",
-    dbContainerID = "",
-    dbCollectionName = ""
+    port: process.env.PORT,
+    host: process.env.HOST,
+    messagebus: process.env.MESSAGE_BUS,
+    environment: process.env.ENVIRONMENT,
+    db: {
+        dbConnectionString: "mongodb://chalumuv-localnewsapplication:TlJ7hnd7iRck25fUFFWYgfJFdK2oSH1N2kbBQjFzb66nqFx486JP6eaCKAQrlyn3Cnwxn6MzJtF5ABeyN9CKYQ==@chalumuv-localnewsapplication.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+    },
+    services:{
+        authentication: process.env.AUTHENTICATION_SERVICE
+    }
 };
 
 module.exports = config;
--- src/services/articles-management/server.js
+++ src/services/articles-management/server.js
@@ -1,2 +1,10 @@
+require('dotenv').config();
+
+let config = require("./environment/config");
+
+
+
+
+
 
 
