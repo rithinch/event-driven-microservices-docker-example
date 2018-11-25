2167b45ae33c541af4f87b67de6c0cde99119a85
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2018-11-25 21:31:00 +0000

    Removed gRPC and mali js packages - to support use of Koa instead

--- src/services/articles-management/server.js
+++ src/services/articles-management/server.js
@@ -1,20 +1,3 @@
 require('dotenv').config();
 const config = require("./environment/config");
 const hostport = `${config.host}:${config.port}`;
-const mali = require("mali");
-const logger = require("mali-logger");
-const SERVICE_INTERFACE_PROTO = "./service/definition.proto"
-const articleManagementService = require("./service/implementation")
-
-let server;
-
-function main(){
-
-    server = new mali(SERVICE_INTERFACE_PROTO);
-    server.use(articleManagementService);
-    server.use(logger());
-    server.start(hostport);
-    console.log(`${server.name} is running on ${hostport}`);
-}
-
-main();
