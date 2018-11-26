a8ae0e41f3d3181d37678a7c580ea1793b8d1cee
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2018-11-26 00:52:15 +0000

    Updated server.js to run Koa server

--- src/services/articles-management/environment/config.js
+++ src/services/articles-management/environment/config.js
@@ -5,7 +5,9 @@ let config = {
     messagebus: process.env.MESSAGE_BUS,
     environment: process.env.ENVIRONMENT,
     db: {
-        dbConnectionString: "mongodb://chalumuv-localnewsapplication:TlJ7hnd7iRck25fUFFWYgfJFdK2oSH1N2kbBQjFzb66nqFx486JP6eaCKAQrlyn3Cnwxn6MzJtF5ABeyN9CKYQ==@chalumuv-localnewsapplication.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+        uri: "mongodb://chalumuv-localnewsapplication.documents.azure.com:10255/?ssl=true&replicaSet=globaldb",
+        username: "chalumuv-localnewsapplication",
+        password: "TlJ7hnd7iRck25fUFFWYgfJFdK2oSH1N2kbBQjFzb66nqFx486JP6eaCKAQrlyn3Cnwxn6MzJtF5ABeyN9CKYQ=="
     },
     services:{
         authentication: process.env.AUTHENTICATION_SERVICE,
--- src/services/articles-management/server.js
+++ src/services/articles-management/server.js
@@ -1,3 +1,22 @@
 require('dotenv').config();
 const config = require("./environment/config");
-const hostport = `${config.host}:${config.port}`;
+
+const koa = require("koa");
+const logger = require("koa-logger");
+const helmet = require("koa-helmet");
+const bodyParser = require("koa-bodyparser");
+const mongoose = require("mongoose");
+
+mongoose.connect(config.db.uri, {user: config.db.username, pass: config.db.password});
+mongoose.connection.on('error', console.error);
+
+const server = new koa();
+
+server.use(logger());
+server.use(bodyParser());
+server.use(helmet());
+
+server.listen(config.port, config.host, () => {
+    console.log(`${config.name} is running at ${config.host}:${config.port}/`)
+});
+
