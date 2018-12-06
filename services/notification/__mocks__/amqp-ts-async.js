6caa9ae4985894b2ccf136ca1ad238ec3171a2b6
Rithin Chalumuri <chalumuv@uni.coventry.ac.uk>
2018-12-06 16:09:59 +0000

    Added Article Subscription and Controller unit tests

--- services/notification/__mocks__/amqp-ts-async.js
+++ services/notification/__mocks__/amqp-ts-async.js
@@ -1,35 +1,35 @@
+/* eslint-disable arrow-body-style */
 /* eslint-disable no-unused-vars */
-const lib = {};
+
 
 const bind = jest.fn((exchange) => {
-
 });
 
 const activateConsumer = jest.fn((onRecieve) => {
   onRecieve();
 });
 
-const stopConsumer = jest.fn(() => {
-});
-
 const declareExchange = jest.fn((exchangeName, type, options) => {
-
+  return exchangeName;
 });
 
-const declareQueue = jest.fn((exchangeName, type, options) => ({
-  bind,
-  activateConsumer,
-}));
+const declareQueue = jest.fn((exchangeName, type, options) => {
+  return {
+    bind,
+    activateConsumer,
+  };
+});
 
-lib.Connection = jest.fn(url => ({
+const connection = jest.fn((url) => {
+  return {
+    connectionUrl: url,
+    declareExchange,
+    declareQueue,
+  };
+});
 
-  connectionUrl: url,
-  declareExchange,
-  declareQueue,
-
-}));
-
-module.exports = lib;
+module.exports.Connection = connection;
 module.exports.activateConsumer = activateConsumer;
 module.exports.bind = bind;
-module.exports.stopConsumer = stopConsumer;
+module.exports.declareExchange = declareExchange;
+module.exports.declareQueue = declareQueue;
